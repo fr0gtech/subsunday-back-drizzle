878c486d6af38e3f55ab09b78a67eece585b4e4a
GREEB <gr44bli@gmail.com>
2025-11-23 02:15:15 +0100

    feat: merge game script

---
+++ mergeGames.ts
@@ -0,0 +1,37 @@
+import { eq } from "drizzle-orm";
+import { db } from "./src/db";
+import { game, vote } from "./src/db/schema";
+
+(async () => {
+    const games = await db.select().from(game);
+
+    const groups = new Map<number, typeof games>();
+
+    for (const g of games) {
+        if (g.steamId === 0) continue; 
+
+        if (!groups.has(g.steamId)) groups.set(g.steamId, []);
+        groups.get(g.steamId)!.push(g);
+    }
+
+    for (const [steamId, group] of groups) {
+        if (group.length <= 1) continue; 
+
+        console.log(
+            `SteamID ${steamId} has duplicates: ${group
+                .map((g) => `${g.name}:${g.id}`)
+                .join(" ")}`
+        );
+
+        const master = group[0];
+        const duplicates = group.slice(1);
+
+        for (const dup of duplicates) {
+            console.log(` → deleting duplicate ${dup.id}`);
+             await db.update(vote)
+                .set({ forId: master?.id })
+                .where(eq(vote.forId, dup.id));
+            await db.delete(game).where(eq(game.id, dup.id))
+        }
+    }
+})();
