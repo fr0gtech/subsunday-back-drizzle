8f984a83f4b798bd3e7e63403bfc7957c83e884d
GREEB <gr44bli@gmail.com>
2026-01-30 14:24:34 +0100

    fix: script add input

--- script/addSteamIdToGame.ts
+++ script/addSteamIdToGame.ts
@@ -9,27 +9,52 @@ import { TZDate } from "@date-fns/tz";
  * 
  * input would be cool to be able to search or at least input wrong game by id and then steamId, it should update the info
  */
-(async () => {
-    const gameToUpdateId = 12
-    const updateTo = 1422450
-    const steamAppDetails = await getInfobyId(updateTo)
-    const moreInfo = (steamAppDetails as any)[updateTo].data;
 
-    await db.update(game).set({
-        name: moreInfo.name,
-        picture: moreInfo.header_image || "",
-        link: "",
-        steamId: updateTo,
-        description: moreInfo.short_description || "",
-        website: moreInfo.website || "",
-        dev: moreInfo.developers || [""],
-        price: moreInfo.is_free ? { final: "free" } : moreInfo.price_overview || { final: "n/a" },
-        categories: moreInfo.genres || {},
-        recommendations: moreInfo.recommendations ? moreInfo.recommendations.total : 0,
-        screenshots: moreInfo.screenshots || [],
-        detailedDescription: JSON.stringify({ html: moreInfo.detailed_description }),
-        movies: moreInfo.movies,
-        createdAt: new TZDate(new Date(), process.env.TIMEZONE),
-        updatedAt: new TZDate(new Date(), process.env.TIMEZONE),
-    }).where(eq(game.id, gameToUpdateId))
+const [num1, num2] = Bun.argv.slice(2).map(Number);;
+
+
+(async () => {
+
+    if (num1 === num2) {
+        throw new Error("cannot be same game")
+    }
+    if (!num1 || !num2) {
+        throw new Error("not both id's provided")
+    }
+
+
+    const gameToUpdateId = num1 as number
+    const updateTo = num2 as number
+    const answer = prompt(`do you want to update game ${gameToUpdateId} to ${updateTo} (y/n)`);
+
+
+    if (answer?.toLowerCase() === 'y' || answer?.toLowerCase() === 'yes') {
+
+        const steamAppDetails = await getInfobyId(updateTo)
+        const moreInfo = (steamAppDetails as any)[updateTo].data;
+
+        await db.update(game).set({
+            name: moreInfo.name,
+            picture: moreInfo.header_image || "",
+            link: "",
+            steamId: updateTo,
+            description: moreInfo.short_description || "",
+            website: moreInfo.website || "",
+            dev: moreInfo.developers || [""],
+            price: moreInfo.is_free ? { final: "free" } : moreInfo.price_overview || { final: "n/a" },
+            categories: moreInfo.genres || {},
+            recommendations: moreInfo.recommendations ? moreInfo.recommendations.total : 0,
+            screenshots: moreInfo.screenshots || [],
+            detailedDescription: JSON.stringify({ html: moreInfo.detailed_description }),
+            movies: moreInfo.movies,
+            createdAt: new TZDate(new Date(), process.env.TIMEZONE),
+            updatedAt: new TZDate(new Date(), process.env.TIMEZONE),
+        }).where(eq(game.id, gameToUpdateId))
+
+    } else {
+        console.log("Cancelled");
+        process.exit(0);
+    }
+
+
 })();
